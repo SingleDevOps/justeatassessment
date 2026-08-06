ff3b96ac29b943c44fcead8ba10eb1b3cd6db45f
writer <60358348+SingleDevOps@users.noreply.github.com>
2026-08-06 23:09:48 +0200

    Upgrade React Native

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,7 @@
 module.exports = {
-  preset: 'react-native',
+  preset: '@react-native/jest-preset',
   setupFiles: ['<rootDir>/jestSetup.js'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
   ],
-  globals: {
-    'ts-jest': {
-      babelConfig: true,
-    },
-  },
 };
